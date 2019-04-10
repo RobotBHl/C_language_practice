bb44e194750892cd4bdecae8cbcd85cf933b3f12
RobotBHl <48479528+RobotBHl@users.noreply.github.com>
2019-04-10 21:03:43 +0800

    2019/03/21

--- daily/main.c
+++ daily/main.c
@@ -1,17 +1,30 @@
+
 #include <stdio.h>
-#include <stdlib.h>
-
-main()
-{
-float sn=100.0,hn=sn/2;
-int n;
-for(n=2;n<=10;n++)
-　{
-　　sn=sn+2*hn;
-　　hn=hn/2;
-　}
-printf("the total of road is %f/n",sn);
-printf("the tenth is %f meter/n",hn);
-return 0:
+int main(){
+    char a, b, c, temp;
+    while(scanf("%c%c%c", &a, &b, &c) != EOF){
+        getchar();//不加getchar()函数不正确
+        if(a > b){
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        //执行完这个if语句之后，a<=b
+        if(a>=c){
+            temp = c;
+            c = b;
+            b = temp;
+            temp = b;
+            b = a;
+            a = temp;
+        }
+        //执行完上边两个if语句之后，a已经是最小的了
+        if(c < b){
+            temp = c;
+            c = b;
+            b = temp;
+        }
+        printf("%c %c %c\n", a, b, c);
+    }
+    return 0;
 }
-
